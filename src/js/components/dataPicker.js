871eaf2fa279cf140ed7ac4e19204eb995406ec9
Tiago Sousa <t.sousa@cheil.com>
2019-07-01 17:17:01 +0100

    Adding calendar to the filters

--- src/js/components/dataPicker.js
+++ src/js/components/dataPicker.js
@@ -1,30 +1,26 @@
-export default function dataPicker() {
+export default function dataPicker(  ) {
 	var dateFormat = "mm/dd/yy",
 
 	from = $( "#from" )
-
 	.datepicker({
 		defaultDate: "+1w",
 		changeMonth: true,
 		numberOfMonths: 3
 	})
-
 	.on( "change", function() {
 		to.datepicker( "option", "minDate", getDate( this ) );
 	}),
-		to = $( "#to" ).datepicker({
+	to = $( "#to" ).datepicker({
 		defaultDate: "+1w",
 		changeMonth: true,
 		numberOfMonths: 3
 	})
-
 	.on( "change", function() {
 		from.datepicker( "option", "maxDate", getDate( this ) );
 	});
 
 	function getDate( element ) {
 		var date;
-
 		try {
 			date = $.datepicker.parseDate( dateFormat, element.value );
 		} catch( error ) {
@@ -33,4 +29,5 @@ export default function dataPicker() {
 
 		return date;
 	}
+
 }
