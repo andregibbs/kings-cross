54d1e36fd6699e7b094add6a3d5b2f52c0ce7f53
Alex Lucas <alex.lucas@cheil.com>
2019-07-25 18:03:05 +0100

    today fixes and images

--- src/js/components/loadingScreen.js
+++ src/js/components/loadingScreen.js
@@ -1,44 +1,34 @@
 export default function loadingScreen(element, animation) {
-    
+  if (document.referrer.includes("explore/kings-cross")) {
+    animationDone();
+  } else {
     var animation = lottie.loadAnimation({
-    container: element,
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    animationData: animation
-  });
+      container: element,
+      renderer: "svg",
+      loop: false,
+      autoplay: true,
+      animationData: animation
+    });
 
-  animation.addEventListener('complete', animationDone);
-
-  function animationDone() {
-    $('#parallax__orangex').addClass('animated fadeInRightBig');
-        $('#parallax__building').addClass('animated fadeInRightBig');
-        $('#parallax__yellowx').addClass('animated fadeInLeftBig');
-        $('#parallax__purplex').addClass('animated fadeInLeftBig');
-        $('.parallax__text').addClass('animated fadeIn');
-    $('.loadingScreen').addClass('done');
-    setTimeout(function () {
-      
-        $('.loadingScreen').hide();
-        
-
-        
-
-
-        }, 400);
-        setTimeout(function () {
-
-          
-  
-          $('#parallax__orangex').removeClass('animated fadeInRightBig');
-        $('#parallax__building').removeClass('animated fadeInRightBig');
-        $('#parallax__yellowx').removeClass('animated fadeInLeftBig');
-        $('#parallax__purplex').removeClass('animated fadeInLeftBig');
-        $('.parallax__text').removeClass('animated fadeIn');
-  
-  
-          }, 1500);
-        
+    animation.addEventListener("complete", animationDone);
   }
 
+  function animationDone() {
+    $("#parallax__orangex").addClass("animated fadeInRightBig");
+    $("#parallax__building").addClass("animated fadeInRightBig");
+    $("#parallax__yellowx").addClass("animated fadeInLeftBig");
+    $("#parallax__purplex").addClass("animated fadeInLeftBig");
+    $(".parallax__text").addClass("animated fadeIn");
+    $(".loadingScreen").addClass("done");
+    setTimeout(function() {
+      $(".loadingScreen").hide();
+    }, 400);
+    setTimeout(function() {
+      $("#parallax__orangex").removeClass("animated fadeInRightBig");
+      $("#parallax__building").removeClass("animated fadeInRightBig");
+      $("#parallax__yellowx").removeClass("animated fadeInLeftBig");
+      $("#parallax__purplex").removeClass("animated fadeInLeftBig");
+      $(".parallax__text").removeClass("animated fadeIn");
+    }, 1500);
+  }
 }
