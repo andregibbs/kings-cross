4693d6c451266da0bd925e9adfddd574adc8ff8f
Reece Tiwari <reece.tiwari@cheil.com>
2019-10-25 15:20:39 +0100

    recently animations

--- src/js/components/discover.js
+++ src/js/components/discover.js
@@ -75,27 +75,103 @@ export default function discover( events ) {
 	});
 
 
-	var tabTriggerBtns = document.querySelectorAll('.tab-item');
+// 	var tabTriggerBtns = document.querySelectorAll('.tab-item');
 
-    tabTriggerBtns.forEach(function(tabTriggerBtn, index){
-     tabTriggerBtn.addEventListener('click', function(){
-		var flyout = document.querySelectorAll('.flyout');
+//     tabTriggerBtns.forEach(function(tabTriggerBtn, index){
+//      tabTriggerBtn.addEventListener('click', function(){
+// 		var flyout = document.querySelectorAll('.flyout');
 		
-        var currentTabData = document.querySelector('.tab-content[data-tab-content="' + this.dataset.tabTrigger + '"]');
+//         var currentTabData = document.querySelector('.tab-content[data-tab-content="' + this.dataset.tabTrigger + '"]');
 
-		// add fade out animation and remove 
+// 		// add fade out animation and remove 
 		
 
+//         // remove classess
+//         document.querySelector('.tab-content.is-open').classList.remove('is-open');
+//         document.querySelector('.is-active').classList.remove('is-active');
+// 		// add classes
 		
-        // remove classess
-        document.querySelector('.tab-content.is-open').classList.remove('is-open');
-        document.querySelector('.is-active').classList.remove('is-active');
-        // add classes
-		currentTabData.classList.add('is-open', 'flyout__on');
-		flyout.classList.add('flyout__on');
-		this.classList.add('flyout', 'is-active');
-		// this.classList.add('flyout__on');
-    });
+// 		currentTabData.classList.add('is-open', 'flyout__on');
+// 		// currentTabData.classList.add('is-open', 'flyout__on');
+// 		this.classList.add('flyout', 'is-active');
+// 		// this.classList.add('flyout__on');
+//     });
+// });
+
+var tabTriggerBtns = document.querySelectorAll('.tab-item');
+
+tabTriggerBtns.forEach(function(tabTriggerBtn, index){
+ tabTriggerBtn.addEventListener('click', function(){
+	
+	var oldImg = $('.floating__image img').attr('src')
+	var newImg = $('.tab-item[data-tab-trigger="' + this.dataset.tabTrigger +'"]')
+	var newImgSrc = $('.tab-item[data-tab-trigger="' + this.dataset.tabTrigger +'"]').attr('src');	
+	var currentTabData = document.querySelector('.tab-content[data-tab-content="' + this.dataset.tabTrigger + '"]');
+	
+	
+
+	console.log('old', oldImg)
+	console.log('new', newImg)
+	// $(newImg).fadeOut(1500, function() {
+	// 	$(".floating__image img").attr('src',newImgSrc);
+		
+	// 	// $(".floating__image").css('visibility','hidden');
+		
+	//    })
+	setTimeout(function(){ 	
+		$(newImg).attr('src',oldImg);
+		// $(newImg).fadeOut(1500, function() {
+		// 	$(".floating__image img").attr('src',newImgSrc);
+			
+		// 	// $(".floating__image").css('visibility','hidden');
+			
+		//    })
+	}, 900);
+
+	
+
+	$('.flyout').not('.floating__image').addClass('flyout__on')
+	
+	
+		$(".floating__image img").attr('src',newImgSrc);
+		
+		// $(".floating__image").css('visibility','hidden');
+		
+	
+	// $('.flyout').not('.floating__image').addClass('flyout__on')
+	$(".floating__image").removeClass('fadeIn');
+	setTimeout(function(){ $('.flyout').removeClass('flyout__on') }, 1000);
+
+
+// 	setTimeout(function(){ 	
+// 		$(".floating__image img").fadeOut(900, function() {
+// 		 $(".floating__image img").attr('src',newImg);
+// 		 $(".floating__image").css('visibility','hidden');
+// 		})
+		
+// }, 500);
+
+setTimeout(function(){ 	
+	// $(".floating__image").css('visibility','initial');
+	$(".floating__image").addClass('fadeIn');
+	$(".floating__image img").show();
+	// $(".floating__image img").fadeIn(1000); 
+}, 1600);
+
+	// add fade out animation and remove 
+	
+
+	
+	// remove classess
+	// document.querySelector('.tab-content.is-open').classList.remove('is-open');
+	// document.querySelector('.is-active').classList.remove('is-active');
+	// add classes
+	
+	// currentTabData.classList.add('is-open', 'flyout__on');
+	
+	// this.classList.add('flyout', 'is-active');
+	
+});
 });
 
 
