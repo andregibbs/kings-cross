2b4e8f1ac49642603132a83e530e36f4b7bcb275
Dan Fuller <dan.fuller@cheil.com>
2020-09-11 16:44:55 +0100

    navigation updates

--- src/js/components/common/kxNav.js
+++ src/js/components/common/kxNav.js
@@ -5,12 +5,26 @@ class KXNav {
     this.el = document.querySelector('.kxNav')
   	this.height = this.el.offsetHeight
     this.sticky = false
+    this.mobileToggle = this.el.querySelector('.kxNav__MobileToggle')
+    this.mobileOpen = false
 
+    // TODO: set mobile nav top offset based on GNB
+    // also add resize/rotate listener to adjust
+
+    this.mobileToggle.addEventListener('click', this.mobileToggleHandler.bind(this))
     window.addEventListener('scroll', this.scrollHandler.bind(this));
   }
 
   getTop() {
     return this.el.getBoundingClientRect().top
+  }
+
+  mobileToggleHandler(e) {
+    if (this.mobileOpen) {
+      this.el.removeAttribute('mobileOpen')
+    } else {
+      this.el.setAttribute('mobileOpen', '')
+    }
   }
 
   scrollHandler(e) {
