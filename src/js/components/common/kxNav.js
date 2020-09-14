2f6d2850d9b9d42f7692aadf6ec9c1bf218e53c2
Dan Fuller <dan.fuller@cheil.com>
2020-09-14 14:02:07 +0100

    nav progress, search and mobile views implemented. +animation

--- src/js/components/common/kxNav.js
+++ src/js/components/common/kxNav.js
@@ -1,3 +1,5 @@
+import navBurgerAnimation from '../../animation/nav-burger-animation.json'
+
 class KXNav {
   constructor() {
     this.search = true; // search instance
@@ -8,43 +10,120 @@ class KXNav {
     this.mobileToggle = this.el.querySelector('.kxNav__MobileToggle')
     this.mobileOpen = false
 
-    // TODO: set mobile nav top offset based on GNB
-    // also add resize/rotate listener to adjust
+    this.searchToggles = [].slice.call(this.el.querySelectorAll('.kxNav__SearchLink'))
+    this.searchOpen = false
+
+    // mobile nav container
+    this.mobileNav = this.el.querySelector('.kxNav__MobileNav')
+    // TODO: add resize/rotate listener to adjust
+
+    // set subview sizes
+    this.setSubviewPosition()
+
+    // create burger animation
+    this.burgerAnimation = this.initBurgerAnimation()
+
+    // create search event
+    this.initSearchToggle()
 
     this.mobileToggle.addEventListener('click', this.mobileToggleHandler.bind(this))
-    window.addEventListener('scroll', this.scrollHandler.bind(this));
+    window.addEventListener('scroll', this.scrollHandler.bind(this))
   }
 
-  getTop() {
-    return this.el.getBoundingClientRect().top
-  }
-
-  mobileToggleHandler(e) {
-    if (this.mobileOpen) {
-      this.el.removeAttribute('mobileOpen')
-    } else {
-      this.el.setAttribute('mobileOpen', '')
-    }
-  }
-
-  scrollHandler(e) {
+  // handles scroll and sets sticky state
+  scrollHandler() {
     if (this.getTop() <= 0) {
-      console.log('sticky')
       this.el.setAttribute('sticky', '')
       this.sticky = true
     } else {
-      console.log('notsticky')
       this.el.removeAttribute('sticky')
       this.sticky = false
     }
   }
 
-  openSearch() {
-    // public exposed search method
+  // gets navigation top position
+  getTop() {
+    return this.el.getBoundingClientRect().top
   }
 
-  closeSearch() {
+  // Sets subview top position (calculating gnb size)
+  setSubviewPosition() {
+    const subViews = [].slice.call(document.querySelectorAll('.kxNav__SubView'))
+    const gnbHeight = document.querySelector('.gnb').offsetHeight
+    subViews.forEach((view) => {
+      view.style.top = `${gnbHeight}px`
+    })
+  }
 
+  // MOBILE NAV
+  // toggle mobile sidebar
+  mobileToggleHandler() {
+    // close search first if open
+    if (this.searchOpen) {
+      return this.closeSearch();
+    }
+    // otherwise toggle mobile nav
+    if (this.mobileOpen) {
+      this.el.removeAttribute('mobile-nav-open')
+      this.mobileOpen = false
+      if (this.burgerAnimation) {
+        this.burgerAnimation.playSegments([95, 125], true);
+      }
+    } else {
+      this.el.setAttribute('mobile-nav-open', '')
+      this.mobileOpen = true
+      if (this.burgerAnimation) {
+        this.burgerAnimation.playSegments([30, 60], true);
+      }
+    }
+  }
+
+  // create burger animation
+  initBurgerAnimation() {
+    const toggleEl = document.querySelector('.kxNav__MobileToggle')
+    let burgerAnimation = false;
+    // burger menu anim
+    if (lottie) {
+      burgerAnimation = lottie.loadAnimation({
+        container: toggleEl, // the dom element that will contain the animation
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        animationData: navBurgerAnimation // the path to the animation json
+      });
+      burgerAnimation.setSpeed(1.3);
+    }
+    return burgerAnimation
+  }
+
+  // SEARCH VIEW
+  // init search view toggle
+  initSearchToggle() {
+    this.searchToggles.forEach(el => {
+      el.addEventListener('click', (e) => {
+        if (this.searchOpen) {
+          this.closeSearch()
+        } else {
+          this.openSearch()
+        }
+      })
+    })
+  }
+
+  // opens search view
+  openSearch() {
+    this.el.setAttribute('search-open', '')
+    this.el.removeAttribute('search-closed')
+    this.searchOpen = true
+  }
+
+  // closes search view
+  closeSearch() {
+    this.el.removeAttribute('search-open')
+    // set a search closed state to animate nav items back
+    // state not set initially on page load
+    this.el.setAttribute('search-closed', '')
+    this.searchOpen = false
   }
 }
 
