fa44854cc8d72352384da439caa8cefc70d3128d
Dan Fuller <dan.fuller@cheil.com>
2020-12-01 09:23:13 +0000

    updates

--- src/data/KX_SURVEY_DATA.js
+++ src/data/KX_SURVEY_DATA.js
@@ -7,7 +7,8 @@
     name of the survey
     wether the surve is active
     url identifier to trigger a form
-    the id of the public google form to embed
+    the url of the google form to embed.
+      (this has been kept as a full url incase anything else needs to be iframed on kx)
 
   this data is used in deploy-kx-survey-data.js
     to deploy the dynamic data for consumption on staging and live envs
@@ -21,7 +22,7 @@ const SURVEY_DATA = [
     name: "NAME OF SURVEY",
     active: false,
     id: "TEST",
-    survey_id: "GOOGLE ID OF FORM",
+    survey_embed_url: "https://docs.google.com/forms/d/e/1FAIpQLScrgghSDPFOhZVlgwZ2RBY9YttF8LO66gp0IV6McTLBhVnOPg/viewform",
     prompt: {
       title: 'KX Survey',
       copy: [
--- src/js/components/common/kxSurvey.js
+++ src/js/components/common/kxSurvey.js
@@ -3,6 +3,7 @@ import getParam from '../getParam' // maybe move replace script
 
 const template = require('../../../templates/partials/components/common/kxSurvey.hbs');
 
+const LOCAL_STORAGE_KEY = 'kxs-history'
 const URL_PARAM = 'kxsid'
 const PROMPT_DISPLAY_DELAY = 1000
 
@@ -20,18 +21,34 @@ const PROMPT_DISPLAY_DELAY = 1000
 class KXSurvey {
   constructor() {
     this.templateTarget = document.querySelector('.cheil-static')
+    this.surveyID = getParam(URL_PARAM)
+
+    this.modalEl = null
+    this.promptEl = null
+    this.promptYes = null
+    this.promptNo = null
+    this.modalClose = null
+
+    // reset view history
+    if (getParam('reset')) {
+      this.resetHistory()
+    }
+
+    if (this.hasBeenAskedForSurvey(this.surveyID) || !this.surveyID) {
+      // user has already been asked about this survey
+      // or no survey id
+      return
+    }
 
     const surveyData = this.getSurveyData()
-
     if (this.templateTarget && surveyData) {
       this.renderTemplate(surveyData)
     }
   }
 
   getSurveyData() {
-    const surveyID = getParam(URL_PARAM)
     const surveyData = SURVEY_DATA.filter(survey => {
-      return survey.id === surveyID
+      return survey.id === this.surveyID
     })[0]
     return surveyData
   }
@@ -48,37 +65,78 @@ class KXSurvey {
 
   setupView() {
     const el = this.templateTarget
-    const modalEl = el.querySelector('.kxSurvey__modal')
-    const promptEl = el.querySelector('.kxSurvey__prompt')
-    const promptYes = el.querySelector('#kxSurvey__prompt--yes')
-    const promptNo = el.querySelector('#kxSurvey__prompt--no')
-    const modalClose = el.querySelector('.kxSurvey__modal_close')
+    this.modalEl = el.querySelector('.kxSurvey__modal')
+    this.promptEl = el.querySelector('.kxSurvey__prompt')
+    this.promptYes = el.querySelector('#kxSurvey__prompt--yes')
+    this.promptNo = el.querySelector('#kxSurvey__prompt--no')
+    this.modalClose = el.querySelector('.kxSurvey__modal_close')
 
-    promptYes.addEventListener('click', () => {
-      console.log('show survey')
-      // lock scroll
-      document.querySelector('html').style.overflow = 'hidden'
-      modalEl.setAttribute('active','')
-      promptEl.removeAttribute('active')
-    })
-    promptNo.addEventListener('click', () => {
-      console.log('no survey')
-      promptEl.removeAttribute('active')
-    })
+    this.promptYes.addEventListener('click', this.userAcceptPrompt.bind(this))
+    this.promptNo.addEventListener('click', this.userRejectPrompt.bind(this))
 
-    modalClose.addEventListener('click', () => {
+    this.modalClose.addEventListener('click', () => {
       // unlock scroll
       document.querySelector('html').style.overflow = ''
-      modalEl.removeAttribute('active')
+      this.modalEl.removeAttribute('active')
     })
 
     // show prompt after x secondss
     setTimeout(() => {
-      promptEl.setAttribute('active', '')
+      this.promptEl.setAttribute('active', '')
     }, PROMPT_DISPLAY_DELAY)
 
-
   }
+
+  userRejectPrompt() {
+    console.log('userRejectPrompt')
+    this.promptEl.removeAttribute('active')
+    this.storeUserAsked()
+  }
+
+  userAcceptPrompt() {
+    console.log('userAcceptPrompt')
+    document.querySelector('html').style.overflow = 'hidden'
+    this.modalEl.setAttribute('active','')
+    this.promptEl.removeAttribute('active')
+    this.storeUserAsked()
+  }
+
+  getSurveyHistory() {
+    const surveyHistory = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (surveyHistory !== null) {
+      // if exists
+      return surveyHistory
+    } else {
+      // else return empty array
+      return []
+    }
+  }
+
+  hasBeenAskedForSurvey(id) {
+    const surveyHistory = this.getSurveyHistory()
+    if (surveyHistory) {
+      // if id is present in stored survey array
+      if (surveyHistory.indexOf(id) > -1) {
+        return true
+      }
+      return false
+    }
+    return false
+  }
+
+  storeUserAsked() {
+    const surveyHistory = this.getSurveyHistory()
+    // push id to stored array
+    surveyHistory.push(this.surveyID)
+    // save array
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(surveyHistory))
+  }
+
+  resetHistory() {
+    console.log('reset storage')
+    localStorage.removeItem(LOCAL_STORAGE_KEY)
+  }
+
 }
 
 
