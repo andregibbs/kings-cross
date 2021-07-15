64b315fd14d32ded0cef8ede6522a55d48170440
Dan Fuller <dan.fuller@cheil.com>
2021-07-15 09:21:56 +0100

    move f1 section to discover, content updates

--- src/js/pages/whats-on/index.js
+++ src/js/pages/whats-on/index.js
@@ -56,12 +56,12 @@ function KX_WhatsOn() {
       launchWorkspace.removeAttribute('active')
       return qudiniBooking.cancelJourney()
     }
-    // hide f1 table before showing calendar
+    // // hide f1 table before showing calendar
     let startDelay = 0
-    if (f1IsActive()) {
-      hideF1Table()
-      startDelay = 600
-    }
+    // if (f1IsActive()) {
+    //   hideF1Table()
+    //   startDelay = 600
+    // }
     // delay qudini open if f1 active
     setTimeout(() => {
       qudiniBooking.start(QudiniFlow.workspace)
@@ -69,79 +69,6 @@ function KX_WhatsOn() {
     }, startDelay)
   })
 
-  // F1
-  const openF1Times = document.querySelector('#open-f1-times')
-  const closeF1Times = document.querySelector('#close-f1-times')
-  const f1TimesEl = document.querySelector('.whatsOn__f1')
-  function showF1Table() {
-    openF1Times.setAttribute('active', '')
-    // get table height to animate container
-    const f1TableHeight = document.querySelector('#f1-wrap').offsetHeight
-    // set active and height
-    f1TimesEl.setAttribute('active', '')
-    f1TimesEl.style.height = `${f1TableHeight}px`
-  }
-  function hideF1Table() {
-    openF1Times.removeAttribute('active')
-    f1TimesEl.removeAttribute('active')
-    f1TimesEl.style.height = `0`
-  }
-  function f1IsActive() {
-    return f1TimesEl.getAttribute('active') !== null
-  }
-  // handle f1 open/close
-  closeF1Times.addEventListener('click', hideF1Table)
-  // f1 times open
-  openF1Times.addEventListener('click', () => {
-    // close any qudini booking before opening f1
-    let openDelay = 0
-    if (qudiniBooking.state.active) {
-      qudiniBooking.cancelJourney()
-      openDelay = 600;
-    }
-    // if already active, close
-    if (f1IsActive()) {
-      return hideF1Table()
-    }
-    // scroll to table
-    setTimeout(() => {
-      $j("html, body").animate({ scrollTop: $j('#f1-wrap').offset().top - 150 }, 600);
-      showF1Table()
-    }, openDelay)
-  })
-
-  // F1 Terms
-  const f1TermsLaunch = document.querySelector('#f1-terms-launch')
-  const f1TermsClose = document.querySelector('#f1-terms-close')
-  const f1TermsContainer = document.querySelector('#f1-terms-container')
-  const f1TermsEl = document.querySelector('.f1Terms')
-  const f1TermsBody = document.querySelector('.f1Terms__body')
-  f1TermsLaunch.addEventListener('click', () => {
-    const termsState = !!f1TermsContainer.getAttribute('active')
-    if (termsState) {
-      f1TermsContainer.removeAttribute('active')
-    } else {
-      f1TermsContainer.setAttribute('active', '')
-    }
-  })
-  f1TermsClose.addEventListener('click', () => {
-    f1TermsContainer.removeAttribute('active')
-  })
-  f1TermsEl.addEventListener('click', () => {
-    f1TermsContainer.removeAttribute('active')
-  })
-  f1TermsBody.addEventListener('click', e => {
-    e.stopPropagation()
-  })
-
-  // delay resize listener to avoid initial resize event
-  setTimeout(() => {
-    window.addEventListener('resize', () => {
-      if (openF1Times.getAttribute('active') != null) {
-        showF1Table()
-      }
-    })
-  }, 1000)
 }
 
 if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
